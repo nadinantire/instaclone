c15085766cc1f7fce93fccc79374f76c705e2dfa
nadinantire <nadinantire@gmail.com>
2019-09-09 13:23:45 +0000

    correction of fovorite link and 404 and 500 error

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
+    def index
+        @favorites=Favorite.all
+    end
+    
     def create
         favorite = current_user.favorites.create(blog_id: params[:blog_id])
         redirect_to blogs_url, notice: "#{favorite.blog.user.name}'s blog has been favorited"
@@ -8,4 +12,5 @@ class FavoritesController < ApplicationController
         favorite = current_user.favorites.find_by(id: params[:id]).destroy
         redirect_to blogs_url, notice: "#{favorite.blog.user.name}'s blog has been unfavored"
     end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "registrations"
   }
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
